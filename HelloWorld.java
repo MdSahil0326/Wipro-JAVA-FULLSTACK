4c2f212f7338f3d55d26c44b86669271cfc4e973
MdSahil0326 <167081054+MdSahil0326@users.noreply.github.com>
2025-07-30 20:04:24 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+      int i_val=10;
+      int j_val=20;
+      boolean chk =i_val<j_val;
+      System.out.println("chk value = " + chk);
+       
+      
+    }
+}
